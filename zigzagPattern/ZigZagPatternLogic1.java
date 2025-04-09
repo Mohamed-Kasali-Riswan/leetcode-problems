3e780daec134498b7c00e15fde2d5b04f4598386
Mohamed-Kasali-Riswan <146856255+Mohamed-Kasali-Riswan@users.noreply.github.com>
2025-04-09 12:47:51 +0530

    Update ZigZagPatternLogic1.java

--- zigzagPattern/ZigZagPatternLogic1.java
+++ zigzagPattern/ZigZagPatternLogic1.java
@@ -3,25 +3,24 @@ package zigzagPattern;
 import java.util.Scanner;
 
 public class ZigZagPatternLogic1 {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String s=sc.next();
-		int numRows=sc.nextInt();
-		System.out.println(generateZigZagPattern(s,numRows));
-	}
+    public static void main(String[] args) {
+	Scanner scanner=new Scanner(System.in);
+	String word=scanner.next();
+	int numRows=scanner.nextInt();
+	System.out.println(generateZigZagPattern(word,numRows));
+    }
 
+    public static String generateZigZagPattern(String word, int numRows) {
+        if (numRows == 1 || word.length() <= numRows) return word;
 
-    public static String generateZigZagPattern(String s, int numRows) {
-        if (numRows == 1 || s.length() <= numRows) return s;
-
-        StringBuilder[] rows = new StringBuilder[numRows];
-        for (int i = 0; i < numRows; i++) rows[i] = new StringBuilder();
+        StringBuilder[] rowAdder = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) rowAdder[i] = new StringBuilder();
 
         int currentRow = 0;
         boolean goingDown = false;
 
-        for (char c : s.toCharArray()) {
-            rows[currentRow].append(c);
+        for (char c : word.toCharArray()) {
+            rowAdder[currentRow].append(c);
 
             if (currentRow == 0 || currentRow == numRows - 1)
                 goingDown = !goingDown;
@@ -29,13 +28,11 @@ public class ZigZagPatternLogic1 {
             currentRow += goingDown ? 1 : -1;
         }
 
-        StringBuilder result = new StringBuilder();
-        for (StringBuilder row : rows) result.append(row);
-        return result.toString();
+        StringBuilder answer = new StringBuilder();
+        for (StringBuilder row : rowAdder) answer.append(row);
+        return answer.toString();
     }
-    
-    
-
+	
 }
 
 
